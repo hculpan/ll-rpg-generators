6ec1ab3d02ab2303bb937444a400c75db91fb83a
Harry Culpan <harry@tesseract.org>
2014-01-07 23:04:31 -0500

    Fixed problem with monster selection in dropdown

--- js/monster-statblock/monster-statblock.js
+++ js/monster-statblock/monster-statblock.js
@@ -4,6 +4,11 @@
 
 var monsterList = new Object();
 
+function monsterClicked(monster) {
+    $("#ms-result").html("Got event for " + monster); // + event.text());
+    $("#ms-dropdownbutton").html(monster + "  <span class='caret'></span>");
+}
+
 $(document).ready(function() {
     jQuery.get('ll_monsters.txt', function(data) {
         var lines = data.split('\n');
@@ -12,16 +17,12 @@ $(document).ready(function() {
         for (var i = 0; i < lines.length; i++) {
             var fields = lines[i].split("|");
             if (fields[0].substr(0, 1) != "#") {
-                $('#ms-monsters').append("<li><a href='#'>" + fields[0] + "</a></li>");
+                $('#ms-monsters').append("<li><a href='javascript:monsterClicked(\"" + fields[0] + "\");'>" + fields[0] + "</a></li>");
                 if (!buttonSet) {
                     $("#ms-dropdownbutton").html(fields[0] + "  <span class='caret'></span>");
                     buttonSet = true;
                 }
             }
         }
-    });
-
-    $(".dropdown-menu li a").click(function(event){
-        $("#ms-result").html("Got " + event.text());
     });
 })
